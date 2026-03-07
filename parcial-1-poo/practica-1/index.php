5b9718abb9b653f72ae8ba7052e73d036167f95b
Jesus-Palacios <jesuspalaciosna07@gmail.com>
2026-03-07 01:32:08 -0700

    Modificación en el archivo index.php

--- parcial-1-poo/practica-1/index.php
+++ parcial-1-poo/practica-1/index.php
@@ -1,3 +1,12 @@
 <?php
+    require_once 'Usuario.php';
+    //Instancia de la clase
+    $ObjUsuario = new Usuario();
 
+    //Asignamos el valor de nombre y lo imprimimos
+    $ObjUsuario->setNombre("Jesús Antonio Palacios Navidad");
+    echo "Nombre: " . $ObjUsuario->getNombre() . "<p>"; 
+    //Asignamos el valor de correo y lo imprimimos
+    $ObjUsuario->setCorreo("jesuspalaciosna07@gmail.com");
+    echo "Correo: " . $ObjUsuario->getCorreo() . "<p>";
 ?>
