2f7e79b2a5f6536993f3c018f5f5dd6b4083d6e5
Jesus-Palacios <jesuspalaciosna07@gmail.com>
2026-03-07 03:27:24 -0700

    Modificación en el archivo index.php

--- parcial-1-poo/practica-2/index.php
+++ parcial-1-poo/practica-2/index.php
@@ -1,3 +1,15 @@
 <?php
+    require_once 'Admin.php';
 
+    //Instancia de la clase
+    $ObjAdmin = new Admin();
+
+    //Asignamos el valor de nombre y lo imprimimos
+    $ObjAdmin->setNombre("Jesús Antonio Palacios Navidad");
+    echo "Nombre: " . $ObjAdmin->getNombre() . "<p>"; 
+    //Asignamos el valor de correo y lo imprimimos
+    $ObjAdmin->setCorreo("jesuspalaciosna07@gmail.com");
+    echo "Correo: " . $ObjAdmin->getCorreo() . "<p>";
+    //Imprime el rol usando el método getRol
+    echo "Rol: " . $ObjAdmin->getRol();
 ?>
