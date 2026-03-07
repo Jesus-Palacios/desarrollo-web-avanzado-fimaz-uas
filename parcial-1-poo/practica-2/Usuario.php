953087cd4529a6a41866f63424eb80a83ca1a101
Jesus-Palacios <jesuspalaciosna07@gmail.com>
2026-03-07 03:02:30 -0700

    Reutilizando código de clase Usuario

--- parcial-1-poo/practica-2/Usuario.php
+++ parcial-1-poo/practica-2/Usuario.php
@@ -1,3 +1,32 @@
 <?php
+        class Usuario {
+        //Atributos privados
+        private $nombre;
+        private $correo;
+        
+        //Constructor, imprime solo el mensaje: "Mi clase se ha inicializado".
+        function __construct(){
+            echo "Mi clase se ha inicializado ! <p>";
+        }
 
+        //Getter nombre
+        public function getNombre(){
+            return $this->nombre;
+        }
+
+        //Getter correo
+        public function getCorreo(){
+            return $this->correo;
+        }
+
+        //Setter nombre
+        public function setNombre($nombre){
+            $this->nombre = $nombre;
+        }
+
+        //Setter correo
+        public function setCorreo($correo){
+            $this->correo = $correo;
+        }
+    }
 ?>
