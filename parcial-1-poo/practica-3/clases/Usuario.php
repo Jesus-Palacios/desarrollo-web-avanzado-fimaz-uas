791fe421884ef381abe382ad1a76226d295778c5
Jesus-Palacios <jesuspalaciosna07@gmail.com>
2026-03-07 23:33:23 -0700

    Modificación el el archivo Usuario.php

--- parcial-1-poo/practica-3/clases/Usuario.php
+++ parcial-1-poo/practica-3/clases/Usuario.php
@@ -1,3 +1,37 @@
 <?php
+    class Usuario {
+        //Atributos privados
+        private $nombre;
+        private $correo;
+        
+        //Constructor, imprime solo el mensaje: "Mi clase se ha inicializado".
+        function __construct(){
+            echo "Mi clase se ha inicializado ! <p>";
+        }
 
+        //Getter nombre
+        public function getNombre(){
+            return $this->nombre;
+        }
+
+        //Getter correo
+        public function getCorreo(){
+            return $this->correo;
+        }
+
+        //Setter nombre
+        public function setNombre($nombre){
+            $this->nombre = $nombre;
+        }
+
+        //Setter para validar el correo
+        public function setCorreo($correo){
+            if (filter_var($correo, FILTER_VALIDATE_EMAIL)){
+                $this->correo = $correo;
+            } else {
+                throw new Exception("Correo inválido");
+            }
+            
+        }
+    }
 ?>
