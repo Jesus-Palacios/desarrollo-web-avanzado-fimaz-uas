defcc1ec350de4a901e8eb0d161edd158d6d89ef
Jesus-Palacios <jesuspalaciosna07@gmail.com>
2026-03-07 19:54:22 -0700

    Se agregaron comentarios en el archivo Admin.php

--- parcial-1-poo/practica-2/Admin.php
+++ parcial-1-poo/practica-2/Admin.php
@@ -1,7 +1,9 @@
 <?php
     require_once "Usuario.php";
     
+    //La clase Admin extiende a la clase Usuario
     class Admin extends Usuario {
+        //La función getRol devuelve "Administrador"
         public function getRol(){
             return "Administrador";
         }
