be6b0d22597b43e4c4b2cd57c05eecbb3347392c
Jesus-Palacios <jesuspalaciosna07@gmail.com>
2026-03-07 23:48:49 -0700

    Modificación el el archivo Alumno.php

--- parcial-1-poo/practica-3/clases/Alumno.php
+++ parcial-1-poo/practica-3/clases/Alumno.php
@@ -1,3 +1,21 @@
 <?php
+    require_once "Usuario.php";
+    class Alumno extends Usuario{
+        private $matricula;
 
+        //Getter matricula
+        public function getMatricula(){
+            return $this->matricula;
+        }
+
+        //Setter matricula
+        public function setMatricula($matricula){
+            $this->matricula = $matricula;
+        }
+
+        //La función getRol devuelve "Alumno"
+        public function getRol(){
+            return "Alumno";
+        }
+    }
 ?>
