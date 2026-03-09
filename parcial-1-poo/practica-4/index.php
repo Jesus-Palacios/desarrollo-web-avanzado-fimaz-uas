244339b256c7b6b87e6aa0ffbc70437ef2bb3c98
Jesus-Palacios <jesuspalaciosna07@gmail.com>
2026-03-09 00:40:00 -0700

    Agrega index con tabla y manejo de excepciones

--- parcial-1-poo/practica-4/index.php
+++ parcial-1-poo/practica-4/index.php
@@ -1,3 +1,58 @@
 <?php
+    require_once 'clases/Admin.php';
+    require_once 'clases/Alumno.php';
+    require_once 'clases/Invitado.php';
 
+    $usuarios = [];
+
+    try {
+
+        $ObjAdmin01 = new Admin("Jesus Palacios", "jesuspalaciosna07@gmail.com");
+        $usuarios[] = $ObjAdmin01;
+
+        $ObjAlumno01 = new Alumno("Victor Estrada", "alexis@gmail.com", 49494);
+        $usuarios[] = $ObjAlumno01;
+
+        $ObjInvitado01 = new Invitado("Jesús Padilla", "jrpadilla@gmail.com", "Coseco");
+        $usuarios[] = $ObjInvitado01;
+        
+        //Prueba de correo mal escrito para probar la excepción
+        $ObjInvitado02 = new Invitado("Pedro Palacios", "pedro32.com", "Esma");
+        $usuarios[] = $ObjInvitado02;
+
+    } catch (Exception $e){
+        echo "Error controlado: " . $e->getMessage();
+    }
 ?>
+
+<table border="1" cellpadding="6" cellspacing="0">
+    <tr>
+        <th>Nombre</th>
+        <th>Correo</th>
+        <th>Matricula</th>
+        <th>Empresa</th>
+        <th>Rol</th>
+    </tr>
+
+    <?php foreach ($usuarios as $tabla) { ?>
+    <tr>
+        <td><?php echo $tabla->getNombre(); ?></td>
+        <td><?php echo $tabla->getCorreo(); ?></td>
+        <td><?php
+            if ($tabla instanceof Alumno) {
+                echo $tabla->getMatricula();
+            } else {
+                echo "Sin datos";
+            } ?>
+        </td> 
+        <td>
+            <?php if ($tabla instanceof Invitado) {
+                echo $tabla->getEmpresa();
+            } else {
+                echo "Sin datos";
+            } ?>
+        </td>
+        <td><?php echo $tabla->getRol(); ?></td>
+    </tr>
+    <?php } ?>
+</table>
