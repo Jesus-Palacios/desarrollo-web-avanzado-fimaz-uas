37d54d86ca995ed20c7e1527f2c21d03911bf17a
Jesus-Palacios <jesuspalaciosna07@gmail.com>
2026-05-08 01:07:52 -0700

    Se modificaciones en el acomodo y colores

--- parcial-3-mvc/examen-practico/views/admin/template/footer.php
+++ parcial-3-mvc/examen-practico/views/admin/template/footer.php
@@ -2,8 +2,8 @@
       </div>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
-  <footer class="text-center text-lg-start bg-white text-muted">
-      <section>
+  <footer class="text-center text-lg-start bg-light text-muted">
+      <section class="p-2">
         <div class="container text-center text-md-start mt-5">
           <div class="row mt-3">
             <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
@@ -23,14 +23,18 @@
             </div>
             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-4">
                 <h6 class="text-uppercase fw-bold mb-4">CONTÁCTO</h6>
-                <p>
-                    CORREO, ETC. 
-                    Facultad de Informática Mazatlán, Universidad Autónoma de Sinaloa.
+                    <!--Estan pendiente de poner iconos-->
+                    <i class="fa-solid fa-phone"> +526691632645</i>
+                    <i class="fa-solid fa-envelope" >jesuspalaciosna07@gmail.com</i> 
+                    <i class="fa-solid fa-school"> Facultad de Informática Mazatlán, Universidad Autónoma de Sinaloa.</i> 
                 </p>
             </div>
           </div>
         </div>
       </section>
+      <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.25);">
+        © Clase Desarrollo Web II. MVC. PHP. PDO. MySQL. BOOTSTRAP.
+      </div>
 
   </footer>
 </html>
