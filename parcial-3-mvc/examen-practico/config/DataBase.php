d80c8a73e7f17dc9abd0a71f34ce3fea24c2cee4
Jesus-Palacios <jesuspalaciosna07@gmail.com>
2026-05-07 18:09:45 -0700

    Se avanzo en config, controllers y models

---
+++ parcial-3-mvc/examen-practico/config/DataBase.php
@@ -0,0 +1,27 @@
+<?php
+    //Crear una clase para conexión a base de datos mediante PDO/
+
+    class Database{
+        //Atributos de la clase Database
+        private $host = "localhost";
+        private $db = "proyecto";
+        private $user = "root";
+        private $password = "";
+
+        public function __construct()
+        {
+           //Constructor... 
+        }
+
+        //Método para conexión a la base de datos.
+        public function connect(){
+            try {
+                $PDO = new PDO ("mysql:host=".$this->host.";dbname=".$this->db,$this->user,
+                $this->password);
+                return $PDO;
+            } catch (PDOException $e) {
+                return $e->getMessage();
+            }
+        }
+    }
+?>
